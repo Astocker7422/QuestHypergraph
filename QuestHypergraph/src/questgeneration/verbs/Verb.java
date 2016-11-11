29b19c13b828c5fb5ce8511b9f3665782212c8d0
Astocker7422 <astocker@mail.bradley.edu>
2016-11-11 11:24:34 -0600

    Update Verb.java

--- QuestHypergraph/src/questgeneration/verbs/Verb.java
+++ QuestHypergraph/src/questgeneration/verbs/Verb.java
@@ -1,13 +1,27 @@
 package questgeneration.verbs;
+
 import questgeneration.nouns.Noun;
+import java.util.ArrayList;
 
 public abstract class Verb
 {
     private final String name;
+    private ArrayList<Verb> predecessor_verbs;
     
     public Verb(String theName)
     {
         name = theName;
+        predecessor_verbs = new ArrayList<Verb>();
+    }
+    
+    public void setPredecessorVerbs(ArrayList<Verb> predecessors)
+    {
+        predecessor_verbs = predecessors;
+    }
+    
+    public ArrayList<Verb> getPredecessorVerbs()
+    {
+        return predecessor_verbs;
     }
     
     public String getName()
@@ -23,4 +37,9 @@ public abstract class Verb
     }
     
     public abstract boolean isCompatible(Noun theNoun);
+    
+    public String toString()
+    {
+        return this.name;
+    }
 }
