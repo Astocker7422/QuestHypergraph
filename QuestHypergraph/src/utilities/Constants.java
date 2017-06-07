e3dde0ea1fe23464d307dca5373cde21e237bc5b
Astocker7422 <astocker@mail.bradley.edu>
2017-06-06 20:56:33 -0500

    Update Constants
    Added a "Method" enum type for parallel hypergraph generation.

--- QuestHypergraph/src/utilities/Constants.java
+++ QuestHypergraph/src/utilities/Constants.java
@@ -5,6 +5,8 @@ public class Constants
 {
     public static final int source_bound = 3;
     
+    public enum Method {BRUTALITY, STEALTH, WEALTH, DIPLOMACY, SORCERY};
+    
     private Constants()
     {
         throw new AssertionError();
