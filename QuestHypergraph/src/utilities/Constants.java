1528782cfc5ad22f3b608ef68699a0b1fc8597d3
Astocker7422 <astocker@mail.bradley.edu>
2017-07-18 19:21:41 -0500

    Update Constants
    Added parallelism_instance_limit to limit the number of sections of parallelism in a single quest. Added parallelism_combination_limit to limit number of combinations of multiple parallelism sub-quests and actions in parallelism generation.

--- QuestHypergraph/src/utilities/Constants.java
+++ QuestHypergraph/src/utilities/Constants.java
@@ -4,6 +4,9 @@ package utilities;
 public class Constants
 {
     public static final int source_bound = 3;
+    public static final int main_quest_nodes_parallelism = 5;
+    public static final int parallelism_instance_limit = 3;
+    public static final int parallelism_combination_limit = 10;
     
     public enum Method {BRUTALITY, STEALTH, WEALTH, DIPLOMACY, SORCERY};
     
