c6629f16dd93e62e9f9608129cfcffc3e1c9e6f3
Astocker7422 <astocker@mail.bradley.edu>
2017-08-29 20:23:52 -0500

    Update Constants
    Changed parameters for specific tests.

--- QuestHypergraph/src/utilities/Constants.java
+++ QuestHypergraph/src/utilities/Constants.java
@@ -5,8 +5,8 @@ public class Constants
 {
     public static final int source_bound = 3;
     public static final int main_quest_nodes_parallelism = 5;
-    public static final int parallelism_instance_limit = 3;
-    public static final int parallelism_combination_limit = 10;
+    public static final int parallelism_instance_limit = 2;
+    public static final int parallelism_combination_limit = 250;
     
     public enum Method {BRUTALITY, STEALTH, WEALTH, DIPLOMACY, SORCERY};
     
